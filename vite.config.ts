7fa2aa9ceea00f32ce9f2b6c7701c94519a414a7
Sutapa Maity <sutapam510@gmail.com>
2025-06-23 22:06:11 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
