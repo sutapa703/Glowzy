1755fe2c7141dd5bc0778d9e02d1e09e74ab30af
Sutapa Maity <sutapam510@gmail.com>
2025-06-23 22:10:11 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: './', // 👈 IMPORTANT: ensures relative paths for production to prevent white screen
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
